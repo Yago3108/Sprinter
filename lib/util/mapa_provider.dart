aa0b085d3267873236bd9bac35989211751a9df2
Yago3108 <yago.ss@aluno.ifsc.edu.br>
2025-09-22 09:43:10 -0300

    Mapa arrumada

--- lib/util/mapa_provider.dart
+++ lib/util/mapa_provider.dart
@@ -15,90 +15,86 @@ class MapaProvider extends ChangeNotifier {
   DateTime? _inicio;
   DateTime? _fim;
   Position? _ultimaPosicao;
-  StreamSubscription<Position>? _stream;
-  MapController? _controller;
-
-  void setController(MapController controller) {
-    _controller = controller;
-  }
 
   void setUid(String uid) {
     _uid = uid;
   }
-
   final MapController controller = MapController(
     initMapWithUserPosition: UserTrackingOption(enableTracking: false),
   );
-  
   StreamSubscription<Position>? _posicaoStream;
 
   void iniciarAtividade() {
     rota.clear();
+    _distancia = 0.0;
+    _tempo = Duration.zero;
+    _inicio = DateTime.now();
+    _ultimaPosicao = null;
 
-    _posicaoStream =
-        Geolocator.getPositionStream(
-          locationSettings: const LocationSettings(
-            accuracy: LocationAccuracy.bestForNavigation, // mais preciso
-            distanceFilter: 0, // pega todos os pontos
-          ),
-        ).listen((pos) async {
-          final ponto = GeoPoint(
-            latitude: pos.latitude,
-            longitude: pos.longitude,
+    _posicaoStream = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.bestForNavigation,
+        distanceFilter: 0,
+      ),
+    ).listen((pos) async {
+      final ponto = GeoPoint(latitude: pos.latitude, longitude: pos.longitude);
+      rota.add(ponto);
+
+      // Atualiza a posição no mapa
+      await controller.moveTo(ponto);
+
+      // Calcula distância acumulada
+      if (_ultimaPosicao != null) {
+        final distanciaLocal = Distance().as(
+          LengthUnit.Meter,
+          LatLng(_ultimaPosicao!.latitude, _ultimaPosicao!.longitude),
+          LatLng(pos.latitude, pos.longitude),
+        );
+        _distancia += distanciaLocal;
+      }
+      _ultimaPosicao = pos;
+
+      // Desenha a rota se houver pelo menos 2 pontos e forem diferentes
+      if (rota.length >= 2) {
+        final GeoPoint start = rota[rota.length - 2];
+        final GeoPoint end = rota.last;
+
+        if (start.latitude != end.latitude || start.longitude != end.longitude) {
+          await controller.drawRoad(
+            start,
+            end,
+            roadOption: const RoadOption(
+              roadColor: Colors.green,
+              roadWidth: 6,
+            ),
           );
-          rota.add(ponto);
+        }
+      }
 
-          // Atualiza a posição no mapa
-          await controller.moveTo(ponto);
-
-          // Desenha a rota se houver pelo menos 2 pontos
-          if (rota.length >= 2) {
-            await controller.drawRoad(
-              rota.first,
-              rota.last,
-              roadOption: const RoadOption(
-                roadColor: Colors.green,
-                roadWidth: 6,
-              ),
-            );
-          }
-
-          notifyListeners();
-        });
+      notifyListeners();
+    });
   }
-
   List<GeoPoint> get rota => _rota;
   double get distancia => _distancia;
   Duration get tempo => _tempo;
 
   Future<void> pararAtividade() async {
     _fim = DateTime.now();
-    _stream?.cancel();
+    _posicaoStream?.cancel();
 
     if (_inicio != null && _fim != null) {
       _tempo = _fim!.difference(_inicio!);
     }
 
     if (_uid != null) {
-      await _firestore
-          .collection('usuarios')
-          .doc(_uid)
-          .collection("atividades")
-          .add({
-            'rota': _rota
-                .map(
-                  (ponto) => {
-                    'latitude': ponto.latitude,
-                    'longitude': ponto.longitude,
-                  },
-                )
-                .toList(),
-            'uid': _uid,
-            'distancia': _distancia,
-            'tempo': _tempo.inSeconds,
-            'inicio': _inicio,
-            'fim': _fim,
-          });
+      await _firestore.collection('usuarios').doc(_uid).collection("atividades").add({
+        'rota': _rota.map((ponto) => {'latitude': ponto.latitude, 'longitude': ponto.longitude}).toList(),
+        'uid': _uid,
+        'distancia': _distancia,
+        'tempo': _tempo.inSeconds,
+        'inicio': _inicio,
+        'fim': _fim,
+      });
     }
 
     notifyListeners();
