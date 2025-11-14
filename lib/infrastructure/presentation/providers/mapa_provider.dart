aa301f82047c7159169ea97f4c5f0a6c4cd8b470
GustavoK1510 <gustavoklc1510@gmail.com>
2025-11-14 10:53:25 -0300

    ícone e caminho

--- lib/infrastructure/presentation/providers/mapa_provider.dart
+++ lib/infrastructure/presentation/providers/mapa_provider.dart
@@ -18,20 +18,18 @@ class MapaProvider extends ChangeNotifier {
   DateTime? _fim;
   DateTime? _ultimoTempo;
   Position? _ultimaPosicao;
+  bool _marcadorInicializado = false;
 
   void setUid(String uid) {
     _uid = uid;
   }
-
   final MapController controller = MapController(
     initMapWithUserPosition: UserTrackingOption(enableTracking: false),
   );
   StreamSubscription<Position>? _posicaoStream;
 
   bool get isAtividadeAtiva => _posicaoStream != null;
-  bool _marcadorInicializado = false;
 
-  //INICIAR ATIVIDADE
   void iniciarAtividade(BuildContext context) {
     rota.clear();
     _distancia = 0.0;
@@ -39,128 +37,142 @@ class MapaProvider extends ChangeNotifier {
     _inicio = DateTime.now();
     _ultimaPosicao = null;
     _ultimoTempo = null;
-    _marcadorInicializado = false;
+    _marcadorInicializado = false; // Garante que a inicialização no mapa ocorrerá
 
-    _posicaoStream =
-        Geolocator.getPositionStream(
-          locationSettings: const LocationSettings(
-            accuracy: LocationAccuracy.bestForNavigation,
-            distanceFilter: 1, // mais fluido
-          ),
-        ).listen((pos) async {
-          final ponto = GeoPoint(
-            latitude: pos.latitude,
-            longitude: pos.longitude,
-          );
+    _posicaoStream = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+         accuracy: LocationAccuracy.bestForNavigation,
+         distanceFilter: 0,
+        ),
+      ).listen((pos) async {
+      final ponto = GeoPoint(latitude: pos.latitude, longitude: pos.longitude);
 
-          // ignora pequenas variações (ruído do GPS)
-          if (_ultimaPosicao != null) {
-            final distanciaLocal = Distance().as(
-              LengthUnit.Meter,
-              LatLng(_ultimaPosicao!.latitude, _ultimaPosicao!.longitude),
-              LatLng(pos.latitude, pos.longitude),
-            );
-            if (distanciaLocal < 3) return; // ignora deslocamentos mínimos
-            _distancia += distanciaLocal;
-          }
+      bool atividadeParada = false;
 
-          rota.add(ponto);
+      if (_ultimaPosicao != null && _ultimoTempo != null) {
 
-          // inicializa marcador do usuário
-          if (!_marcadorInicializado) {
-            await controller.setStaticPosition([ponto], 'userPosition');
-            await controller.setMarkerOfStaticPoint(
-              id: 'userPosition',
-              markerIcon: MarkerIcon(
-                iconWidget: Icon(
-                  Icons.person_pin_circle,
-                  color: Colors.blue,
-                  size: 48,
+        final ultimaPosicaoValida = _ultimaPosicao!; 
+        final ultimoTempoValido = _ultimoTempo!;
+
+        final distanciaLocal = Distance().as(
+          LengthUnit.Meter,
+          LatLng(ultimaPosicaoValida.latitude, ultimaPosicaoValida.longitude),
+          LatLng(pos.latitude, pos.longitude),
+        );
+
+        final Duration tempoLocal = pos.timestamp.difference(ultimoTempoValido);
+
+        if (tempoLocal.inSeconds > 0) {
+          final double velocidade = distanciaLocal / tempoLocal.inSeconds;
+          final double velocidadeKm = velocidade * 3.6;
+
+          if (velocidadeKm > 40) {
+            atividadeParada = true;
+
+            await pararAtividade(context);
+
+            if (ModalRoute.of(context)?.isCurrent ?? false) {
+              showDialog(
+                context: context,
+                builder: (ctx) => AlertDialog(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  title: Row(children: [
+                    const Icon(Icons.warning_amber_rounded, color: Color.fromARGB(255, 5, 106, 12), size: 30),
+                    const SizedBox(width: 10),
+                    Text("Alerta de Velocidade!", style: TextStyle(color: Color.fromARGB(255, 5, 106, 12), fontWeight: FontWeight.bold)),
+                  ]),
+                  content: const Text("Você está andando acima da velocidade permitida do aplicativo! Diminua a velocidade e reinicie a atividade.", style: TextStyle(fontSize: 16)),
+                  actions: [
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 5, 106, 12),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      ),
+                      onPressed: () => Navigator.of(ctx).pop(),
+                      child: const Text("Entendi", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                    )
+                  ],
                 ),
-              ),
-            );
-            _marcadorInicializado = true;
-          } else {
-            // move suavemente o marcador existente entre a posição antiga e a nova
-            if (_ultimaPosicao != null) {
-              final oldLocation = GeoPoint(
-                latitude: _ultimaPosicao!.latitude,
-                longitude: _ultimaPosicao!.longitude,
               );
-              await controller.changeLocationMarker(
-                oldLocation: oldLocation,
-                newLocation: ponto,
-              );
-            } else {
-              // fallback (primeira atualização)
-              await controller.setStaticPosition([ponto], 'userPosition');
             }
-          }
-
-          // atualiza a rota desenhada
-          if (rota.length >= 2) {
-            await controller.drawRoadManually(
-              rota,
-              const RoadOption(roadColor: Colors.green, roadWidth: 6),
-            );
-          }
-
-          // atualiza tempo total
-          if (_ultimoTempo != null) {
-            final Duration tempoLocal = pos.timestamp.difference(_ultimoTempo!);
-            if (tempoLocal.inSeconds > 0) {
-              _tempo += tempoLocal;
-            }
-          }
-
-          _ultimaPosicao = pos;
-          _ultimoTempo = pos.timestamp;
 
           notifyListeners();
-        });
-  }
+          return; 
+        }
 
+          _distancia += distanciaLocal;
+          _tempo += tempoLocal; // Atualiza o tempo total decorrido
+      }
+      }
+
+      if (!atividadeParada) {
+        rota.add(ponto); 
+        _ultimaPosicao = pos;
+        _ultimoTempo = pos.timestamp;
+
+        if (!_marcadorInicializado) {
+          await controller.setStaticPosition([ponto], 'userPosition');
+          await controller.setMarkerOfStaticPoint(
+            id: 'userPosition',
+            markerIcon: MarkerIcon(
+              iconWidget: Icon(Icons.person_pin_circle, color: Colors.blue, size: 48),
+            ),
+          );
+          await controller.goToLocation(ponto);
+          _marcadorInicializado = true;
+        } else {
+          await controller.setStaticPosition([ponto], 'userPosition');
+        }
+
+        await controller.moveTo(ponto); 
+
+        if (rota.length >= 2) {
+          await controller.drawRoadManually(
+            rota,
+            const RoadOption(roadColor: Colors.green, roadWidth: 6, zoomInto: false),
+          );
+        }
+      }
+
+      notifyListeners();
+    });
+  }
   List<GeoPoint> get rota => _rota;
   double get distancia => _distancia;
   Duration get tempo => _tempo;
 
-  //PARAR ATIVIDADE
   Future<void> pararAtividade(BuildContext context) async {
     _fim = DateTime.now();
     _posicaoStream?.cancel();
     _posicaoStream = null;
 
+    if (_inicio != null && _fim != null) {
+      _tempo = _fim!.difference(_inicio!);
+    }
+
     double fatorEmissao = 1.5;
 
-    double emissao = fatorEmissao * (_distancia / 1000);
+    double emissao = _tempo.inSeconds*fatorEmissao*(_distancia/1000);
+    
+    int pontos = (emissao/100).floor();
+    
 
-    int pontos = (emissao / 5).floor();
-
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
-    userProvider.atualizarCC(pontos, emissao, distancia / 1000);
+    final userProvider = Provider.of<UserProvider>(context,listen: false);
+    userProvider.atualizarCC(pontos, emissao, distancia/1000);
 
     if (_uid != null) {
-      await _firestore
-          .collection('usuarios')
-          .doc(_uid)
-          .collection("atividades")
-          .add({
-            'rota': _rota
-                .map(
-                  (ponto) => {
-                    'latitude': ponto.latitude,
-                    'longitude': ponto.longitude,
-                  },
-                )
-                .toList(),
-            'uid': _uid,
-            'distancia': _distancia,
-            'tempo': _tempo.inSeconds,
-            'inicio': _inicio,
-            'fim': _fim,
-            'emissao': emissao,
-            'pontos': pontos,
-          });
+      await _firestore.collection('usuarios').doc(_uid).collection("atividades").add({
+        'rota': _rota.map((ponto) => { 'latitude': ponto.latitude, 'longitude': ponto.longitude}).toList(),
+        'uid': _uid,
+        'distancia': _distancia,
+        'tempo': _tempo.inSeconds,
+        'inicio': _inicio,
+        'fim': _fim,
+        'emissao': emissao,
+        'pontos': pontos
+      });
     }
 
     notifyListeners();
